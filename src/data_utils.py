5a0f8dc15b749d6c9fcc88e5322a91dfd7885fdd
Madhan M <varadhodiyil@gmail.com>
2019-11-24 21:02:57 +0000

    [added] Data Utils

---
+++ src/data_utils.py
@@ -0,0 +1,31 @@
+import json
+import os
+import datetime
+class MyExceptions(Exception):
+    def __init__(self, message,*args, **kwargs):
+        super().__init__(message,*args, **kwargs)
+        self.message = message
+
+    def __str__(self):
+        return  "Uh -huh {0} Error Occured at {1}" .format(self.message ,datetime.datetime.now())
+
+
+class DataUtils():
+    def __init__(self,in_file=None):
+        self.in_file = in_file
+
+    def read_data(self,in_file = None):
+        if self.in_file is None and in_file is None:
+            raise MyExceptions("Please specify input file please")
+        data_file = self.in_file if self.in_file else in_file
+        if not os.path.exists(data_file):
+            raise MyExceptions("File Doesn't exist")
+        f = open(data_file,"r")
+        return json.load(f)
+
+
+
+d = DataUtils(in_file="../data/training/d13f34041.json")
+
+
+d.read_data()
