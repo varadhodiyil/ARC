a74f09ad525a8c7116b73eb35d35e7232ae2e5ff
gnithie <g.nithie@gmail.com>
2019-11-27 02:51:45 +0000

    Graphical changes to show Input and output grids

--- src/2281f1f4.py
+++ src/2281f1f4.py
@@ -32,8 +32,21 @@ class ARC:
 			else:
 				print('wrong')
 			# cmap = matplotlib.colors.LinearSegmentedColormap('my_colormap', segmentdata=cdict,N=256)
-			plt.matshow(result_data,  vmin=0, vmax=9, cmap=ListedColormap(
+			fig, axes = plt.subplots(ncols=2, figsize=(8,8))
+			ax1, ax2 = axes
+
+			ax1.matshow(inputArr, vmin=0, vmax=9,  cmap=ListedColormap(
 				['#000000', '#0074D9', '#FF4136','#2ECC40','#FFDC00','#AAAAAA','#F012BE','#FF851B','#7FDBFF','#870C25']))
+			ax2.matshow(result_data, vmin=0, vmax=9, cmap=ListedColormap(
+				['#000000', '#0074D9', '#FF4136','#2ECC40','#FFDC00','#AAAAAA','#F012BE','#FF851B','#7FDBFF','#870C25']))
+			# plt.colorbar()
+			ax1.set_xticks(np.arange(-.5, 9, 1), minor=True)
+			ax1.set_yticks(np.arange(-.5, 9, 1), minor=True)
+			# ax1.Text('Inut')
+			ax2.set_xticks(np.arange(-.5, 9, 1), minor=True)
+			ax2.set_yticks(np.arange(-.5, 9, 1), minor=True)
+			ax1.grid(which='minor', color='w', linestyle='-', linewidth=0.5)
+			ax2.grid(which='minor', color='w', linestyle='-', linewidth=0.5)
 			plt.show()
 arc = ARC()
 data = arc.read_data(filename = "data/training/2281f1f4.json")
