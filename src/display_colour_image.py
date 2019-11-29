d41363b8b0f4d657553ea647be1683ad7fe2519b
gnithie <g.nithie@gmail.com>
2019-11-29 17:54:24 +0000

    [fixed] generalized the code

--- src/display_colour_image.py
+++ src/display_colour_image.py
@@ -7,27 +7,29 @@ class DisplayColourImage():
     cmap=ListedColormap(['#000000', '#0074D9', '#FF4136', '#2ECC40', '#FFDC00', '#AAAAAA', 
                         '#F012BE', '#FF851B', '#7FDBFF', '#870C25'])
 
-    def __init__(self, input_array, output_array):
+    def __init__(self, name, input_array, output_array):
+        self.name = name
         self.input_array = input_array
         self.output_array = output_array
 
     def display_data(self):
         fig, axes = plt.subplots(ncols=2, figsize=(8, 8))
         ax1, ax2 = axes
-        input_size = self.input_array.shape[0]-1
-        output_size = self.output_array.shape[0]-1
-        ax1.matshow(self.input_array, vmin=0, vmax=9,  cmap=self.cmap)
-        ax2.matshow(self.output_array, vmin=0, vmax=9, cmap=ListedColormap(
-            ['#000000', '#0074D9', '#FF4136', '#2ECC40', '#FFDC00', '#AAAAAA', '#F012BE', '#FF851B', '#7FDBFF', '#870C25']))
-        ax1.set_xticks(np.arange(-.5, input_size, 1), minor=True)
-        ax1.set_yticks(np.arange(-.5, input_size, 1), minor=True)
-        ax2.set_xticks(np.arange(-.5, output_size, 1), minor=True)
-        ax2.set_yticks(np.arange(-.5, output_size, 1), minor=True)
-        ax1.grid(which='minor', color='w', linestyle='-', linewidth=0.5)
-        ax2.grid(which='minor', color='w', linestyle='-', linewidth=0.5)
+        fig.suptitle('Task '+ self.name)
+        ax1 = self.set_plot(ax1, self.input_array, 'Input Grid')
+        ax2 = self.set_plot(ax2, self.output_array, 'Output Grid')
         plt.show()
 
-    
+    def set_plot(self, axis, array, title):
+        size = array.shape[0]-1
+        axis.matshow(array, vmin=0, vmax=9,  cmap=self.cmap)
+        axis.set_xticks(np.arange(-.5, size, 1), minor=True)
+        axis.set_yticks(np.arange(-.5, size, 1), minor=True)
+        axis.set_xticklabels([])
+        axis.set_yticklabels([])
+        axis.set_title(title)
+        axis.grid(which='minor', color='w', linestyle='-', linewidth=0.5)
+        return axis
 
 
 
