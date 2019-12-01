8fde51c4b6a856cc6d1b036dfe85fd2bacc231fa
saurabhhebbalkar5 <saurabhhebbalkar@gmail.com>
2019-12-01 21:00:10 +0000

    Made changes to display graphics

--- src/Solution_67e8384a.py
+++ src/Solution_67e8384a.py
@@ -7,8 +7,12 @@ Created on Wed Nov 20 15:48:49 2019
 import json
 import numpy as np
 from data_utils import DataUtils, MyExceptions
+from display_colour_image import DisplayColourImage
 
 class ARC:
+
+    def __init__(self,graphics=False):
+        self.graphics = graphics
     
     def read_data(self, filename):
         data = open(filename, 'r')
@@ -22,7 +26,10 @@ class ARC:
 
             inputArr = np.asfarray(inputData, dtype=int)
             result_data = self.solve(inputArr)
-            #print(inputArr)        
+            result_data = np.array(result_data)
+            inputData = np.array(inputData)
+            if self.graphics:
+                DisplayColourImage("Solution for 67e8384a Task:", inputData, result_data).display_data()      
             print(result_data)
             print("\n")
 
@@ -38,8 +45,9 @@ if __name__ == "__main__":
     import sys
     if len(sys.argv) < 2:
         raise MyExceptions("Please Specify a valid file path")
+    graphics = sys.argv[2] if len(sys.argv) ==3 else False
     path = sys.argv[1]
-    arc = ARC()
+    arc = ARC(graphics=graphics)
     data = arc.read_data(filename = path)
     arc.read_train(data)
 
